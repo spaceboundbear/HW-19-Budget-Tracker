944c6bb65986dc25fe5e81aa01a0425586acfeeb
spaceboundbear <mf.michaelfischer@gmail.com>
2021-12-10 18:14:11 -0600

    fighting heroku

--- server.js
+++ server.js
@@ -18,8 +18,6 @@ app.use(express.static('public'));
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
   useNewUrlParser: true,
   useFindAndModify: false,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
 });
 
 // routes
